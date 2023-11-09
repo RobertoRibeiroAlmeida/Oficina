fd0e6539b273d3d5d2a42d6e186028bf1f07eb2f
Roberto Ribeiro de Almeida <roberto.ralmeida@gmail.com>
2023-11-09 16:53:12 -0300

    Ajustes no core models

--- core/models.py
+++ core/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 
 
+UNIDADE_CHOICES = (
+        ("UN", "Unidade"),
+        ("MT", "Metro linear"),
+        ("M2", "Metro quadrado"),
+        ("M3", "Metro cubico"),
+        ("L", "Litro"),
+        ("KG", "Quilograma")
+)
+
+
 class GrupoProduto(models.Model):
     nome = models.CharField(max_length=50, null=False, blank=False)
 
@@ -9,8 +19,25 @@ class GrupoProduto(models.Model):
 
 
 class Produto(models.Model):
-    marca = models.ForeignKey(GrupoProduto, on_delete=models.CASCADE)
-    nome = models.CharField(max_length=50)
+    grupo = models.ForeignKey(GrupoProduto, on_delete=models.CASCADE)
+    nome = models.CharField(max_length=150, blank=False, null=False)
+    unidade = models.CharField(max_length=2, choices=UNIDADE_CHOICES, blank=False, null=False)
 
     def __str__(self):
         return self.nome
+
+
+class Especialidades(models.Model):
+    nome = models.CharField(max_length=50, null=False, blank=False)
+
+    def __str__(self):
+        return self.nome
+
+
+class Profissional(models.Model):
+    nome = models.CharField(max_length=150, null=False, blank=False)
+    cpf = models.CharField(max_length=20, null=False, blank=False)
+    celular1 = models.CharField(max_length=20, null=True, blank=True)
+    celular2 = models.CharField(max_length=20, null=True, blank=True)
+    especialidade = models.ForeignKey(Especialidades, on_delete=models.CASCADE)
+
