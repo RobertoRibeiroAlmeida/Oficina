4b7f03b1433c6c6cc73b5fe1012585783d385da1
Roberto Ribeiro de Almeida <roberto.ralmeida@gmail.com>
2023-11-16 13:02:37 -0300

    Ajustes no core models

--- oficina/urls.py
+++ oficina/urls.py
@@ -25,6 +25,6 @@ urlpatterns = [
     path('veiculos/', include('veiculos.urls', namespace='veiculos')),
     path('core/', include('core.urls', namespace='core')),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('admin/', admin.site.urls),
 ]
